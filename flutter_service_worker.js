19b8adda89aecbd7079038556210510e2a6932e1
Ananth <44719037+infinityp913@users.noreply.github.com>
2020-07-04 11:59:54 -0400

    About section and Font
    Added new about section, new font (StayWriter) and comments

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "4d467455105dec9e96b2b8acbb9cb0fe",
+  "assets/AssetManifest.json": "9c04879a19d6ee31565335a509bf6224",
 "assets/assets/background_image.png": "fa3d365544c887c850416402ce3c399b",
 "assets/assets/fb-icon.png": "e64ad7aaa7950dc4f60c5d312189e9ba",
 "assets/assets/insta-icon.png": "005c9b1663a859b8400121d7c87e6a79",
@@ -10,8 +10,10 @@ const RESOURCES = {
 "assets/assets/pufferspondpic1.jpg": "6c5ae02cf9d0750f2ad8a34e5d259e0c",
 "assets/assets/pufferspondpic2.jpg": "56cfc7e2bbd4f122473a611d5ed8838b",
 "assets/assets/twitter-icon.png": "8e3f84f50e8968fdb29f3a2644c6022e",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "f10a489fd3b3857488f56dbeb50df0de",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/fonts/StayWriter-Bold.otf": "6810cd1b4e98175de0a8d3bc03c8e0bd",
+"assets/fonts/StayWriter-Handmade.otf": "5394e488427b5304a64dab2ad03cc0f7",
 "assets/LICENSE": "53116e3cc00063786165baf6fc00ec20",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -19,7 +21,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "4d757bb50a255d223bec628fe57f4572",
 "/": "4d757bb50a255d223bec628fe57f4572",
-"main.dart.js": "a0b3a3cdea23834728fa0d8abb210142",
+"main.dart.js": "552a454912b7c2a96ee332ababa8b4e2",
 "manifest.json": "0d1600c2a888fa4f14d6215b200b6351"
 };
 
